4ead6937e8a4550a704c0fa14cd4750ff3033ef8
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-07-19 23:43:33 +0900

    css 변동사항 체크

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const handleDrop = (e) => {
   const tempImagePath = URL.createObjectURL(e.dataTransfer.files[0]);
   document.querySelector('#uploaded').src = tempImagePath;
   setCropper();
-  document.querySelector('.input-div').style.display = 'none';
+  document.querySelector('.input-area').style.display = 'none';
 }
 
 const handleChange = (e) => {
@@ -145,8 +145,46 @@ const reset = () => {
 
 const showCropper = () => {
   setCropper();
-  document.querySelector('.input-div').style.display = 'none';
+  document.querySelector('.input-area').style.display = 'none';
   document.querySelector('.camera-area').style.display = 'none';
+}
+
+// camera
+const video = document.querySelector('.camera-area video');
+
+const setCamera = () => {
+  document.querySelector('.camera-area').style.display = 'block';
+  document.querySelector('.input-area').style.display = 'none';
+
+  navigator.mediaDevices
+  .getUserMedia({ video: true, audio: false })
+  .then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+const setInput = () => {
+  document.querySelector('.camera-area').style.display = 'none';
+  document.querySelector('.input-area').style.display = 'block';
+
+  video.srcObject = null;
+}
+
+const takePicture = () => {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext("2d");
+  const width = 1024;
+  const height = 540;
+  canvas.width = width;
+  canvas.height = height;
+  context.drawImage(video, 0, 0, width, height);
+
+  document.querySelector('#uploaded').src = canvas.toDataURL();
+  showCropper();
 }
 
 var Module = {
